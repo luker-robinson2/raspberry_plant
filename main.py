d2901358f07bc4b67f0f19a5892c7321a614801e
Luke Robinson <lukerob20042@gmail.com>
2024-10-14 23:58:35 -0600

    led

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ import time
 # Set the pin numbering mode
 GPIO.setmode(GPIO.BOARD)
 
+led_pin = 17
+
 # Set up the GPIO pin (pin 11 in this case) as an output
-GPIO.setup(0, GPIO.OUT)
+GPIO.setup(led_pin, GPIO.OUT)
 
 try:
     while True:
-        GPIO.output(0, GPIO.HIGH)  # Turn on the LED
+        GPIO.output(led_pin, GPIO.HIGH)  # Turn on the LED
         time.sleep(1)               # Wait for 1 second
-        GPIO.output(0, GPIO.LOW)   # Turn off the LED
+        GPIO.output(led_pin, GPIO.LOW)   # Turn off the LED
         time.sleep(1)               # Wait for 1 second
 except KeyboardInterrupt:
     GPIO.cleanup()  # Clean up GPIO on CTRL+C exit
